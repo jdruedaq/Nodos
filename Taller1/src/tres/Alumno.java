028c6d64566b466be433c61168359e5f06c0d059
Juan David Rueda Quiroga (CTO) <jrueda@oneago.com>
2019-10-31 14:34:04 -0500

    Fixed bugs

--- Taller1/src/tres/Alumno.java
+++ Taller1/src/tres/Alumno.java
@@ -3,14 +3,16 @@ package tres;
 public class Alumno {
     public static final int MEN = 0;
     public static final int FEMALE = 1;
-    String name;
-    int age;
-    int gender;
+    private long dni;
+    private String name;
+    private int age;
+    private int gender;
 
-    public Alumno(String name, int age, @IntRange(minimo = 0, maximo = 1) int gender) {
+    public Alumno(long dni, String name, int age, @IntRange(minimo = 0, maximo = 1) int gender) {
         if (gender > 1) {
             return;
         }
+        this.dni = dni;
         this.name = name;
         this.age = age;
         this.gender = gender;
@@ -38,5 +40,13 @@ public class Alumno {
 
     public void setGender(@IntRange(minimo = 0, maximo = 1) int gender) {
         this.gender = gender;
+    }
+
+    public long getDni() {
+        return dni;
+    }
+
+    public void setDni(long dni) {
+        this.dni = dni;
     }
 }
